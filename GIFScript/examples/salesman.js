9bb7f1bd5cd6016f3f0825f03ac9b89f6063e844
abyardim <batuhan.yardim@gmail.com>
2016-06-28 00:25:24 +0300

    Implement genetic TSP solver example

--- GIFScript/examples/salesman.js
+++ GIFScript/examples/salesman.js
@@ -1,10 +1,448 @@
-setDim( 1024, 500);
+
+
+////////////////// genetic algorithm from https://github.com/parano/GeneticAlgorithm-TSP
+
+/*The MIT License (MIT)
+
+Copyright (c) 2016 Chaoyu Yang <paranoyang@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/ 
+
+/////////////////// utils
+
+Array.prototype.clone = function() { return this.slice(0); }
+Array.prototype.shuffle = function() {
+  for(var j, x, i = this.length-1; i; j = randomNumber(i), x = this[--i], this[i] = this[j], this[j] = x);
+  return this;
+};
+Array.prototype.indexOf = function (value) {	
+  for(var i=0; i<this.length; i++) {
+    if(this[i] === value) {
+      return i;
+    }
+  }
+}
+Array.prototype.deleteByValue = function (value) {
+  var pos = this.indexOf(value);
+  this.splice(pos, 1);
+}
+Array.prototype.next = function (index) {
+  if(index === this.length-1) {
+    return this[0];
+  } else {
+    return this[index+1];
+  }
+}
+Array.prototype.previous = function (index) {
+  if(index === 0) {
+    return this[this.length-1];
+  } else {
+    return this[index-1];
+  }
+}
+Array.prototype.swap = function (x, y) {
+  if(x>this.length || y>this.length || x === y) {return}
+  var tem = this[x];
+  this[x] = this[y];
+  this[y] = tem;
+}
+Array.prototype.roll = function () {
+  var rand = randomNumber(this.length);
+  var tem = [];
+  for(var i = rand; i<this.length; i++) {
+    tem.push(this[i]);
+  }
+  for(var i = 0; i<rand; i++) {
+    tem.push(this[i]);
+  }
+  return tem;
+}
+Array.prototype.reject = function (array) {
+  return $.map(this,function (ele) {
+    return $.inArray(ele, array) < 0 ? ele : null;
+  })
+}
+function intersect(x, y) {
+  return $.map(x, function (xi) {
+    return $.inArray(xi, y) < 0 ? null : xi;
+  })
+}
+Point = Point2;
+function randomPoint() {
+  var randomx = randomNumber(WIDTH);
+  var randomy = randomNumber(HEIGHT);
+  var randomPoint = new Point(randomx, randomy);
+  return randomPoint; 
+}
+function randomNumber(boundary) {
+  return parseInt(Math.random() * boundary);
+  //return Math.floor(Math.random() * boundary);
+}
+function distance(p1, p2) {
+  return euclidean(p1.x-p2.x, p1.y-p2.y);
+}
+function euclidean(dx, dy) {
+  return Math.sqrt(dx*dx + dy*dy);
+}
+
+//////////////////////////////////
+
+//////// algorithm.js
+
+function GAInitialize() {
+  countDistances();
+  for(var i=0; i<POPULATION_SIZE; i++) {
+    population.push(randomIndivial(points.length));
+  }
+  setBestValue();
+}
+function GANextGeneration() {
+  currentGeneration++;
+  selection();
+  crossover();
+  mutation();
+
+  //if(UNCHANGED_GENS > POPULATION_SIZE + ~~(points.length/10)) {
+    //MUTATION_PROBABILITY = 0.05;
+    //if(doPreciseMutate) {
+    //  best = preciseMutate(best);
+    //  best = preciseMutate1(best);
+    //  if(evaluate(best) < bestValue) {
+    //    bestValue = evaluate(best);
+    //    UNCHANGED_GENS = 0;
+    //    doPreciseMutate = true;
+    //  } else {
+    //    doPreciseMutate = false;
+    //  }
+    //}
+  //} else {
+    //doPreciseMutate = 1;
+    //MUTATION_PROBABILITY = 0.01;
+  //}
+  setBestValue();
+}
+function tribulate() {
+  //for(var i=0; i<POPULATION_SIZE; i++) {
+  for(var i=population.length>>1; i<POPULATION_SIZE; i++) {
+    population[i] = randomIndivial(points.length);
+  }	
+}
+function selection() {
+  var parents = new Array();
+  var initnum = 4;
+  parents.push(population[currentBest.bestPosition]);
+  parents.push(doMutate(best.clone()));
+  parents.push(pushMutate(best.clone()));
+  parents.push(best.clone());
+
+  setRoulette();
+  for(var i=initnum; i<POPULATION_SIZE; i++) {
+    parents.push(population[wheelOut(Math.random())]);
+  }
+  population = parents;
+}
+function crossover() {
+  var queue = new Array();
+  for(var i=0; i<POPULATION_SIZE; i++) {
+    if( Math.random() < CROSSOVER_PROBABILITY ) {
+      queue.push(i);
+    }
+  } 
+  queue.shuffle();
+  for(var i=0, j=queue.length-1; i<j; i+=2) {
+    doCrossover(queue[i], queue[i+1]);
+    //oxCrossover(queue[i], queue[i+1]);
+  }
+}
+//function oxCrossover(x, y) {	
+//  //var px = population[x].roll();
+//  //var py = population[y].roll();
+//  var px = population[x].slice(0);
+//  var py = population[y].slice(0);
+
+//  var rand = randomNumber(points.length-1) + 1;
+//  var pre_x = px.slice(0, rand);
+//  var pre_y = py.slice(0, rand);
+
+//  var tail_x = px.slice(rand, px.length);
+//  var tail_y = py.slice(rand, py.length);
+
+//  px = tail_x.concat(pre_x);
+//  py = tail_y.concat(pre_y);
+
+//  population[x] = pre_y.concat(px.reject(pre_y));
+//  population[y] = pre_x.concat(py.reject(pre_x));
+//}
+function doCrossover(x, y) {
+  child1 = getChild('next', x, y);
+  child2 = getChild('previous', x, y);
+  population[x] = child1;
+  population[y] = child2;
+}
+function getChild(fun, x, y) {
+  solution = new Array();
+  var px = population[x].clone();
+  var py = population[y].clone();
+  var dx,dy;
+  var c = px[randomNumber(px.length)];
+  solution.push(c);
+  while(px.length > 1) {
+    dx = px[fun](px.indexOf(c));
+    dy = py[fun](py.indexOf(c));
+    px.deleteByValue(c);
+    py.deleteByValue(c);
+    c = dis[c][dx] < dis[c][dy] ? dx : dy;
+    solution.push(c);
+  }
+  return solution;
+}
+function mutation() {
+  for(var i=0; i<POPULATION_SIZE; i++) {
+    if(Math.random() < MUTATION_PROBABILITY) {
+      if(Math.random() > 0.5) {
+        population[i] = pushMutate(population[i]);
+      } else {
+        population[i] = doMutate(population[i]);
+      }
+      i--;
+    }
+  }
+}
+function preciseMutate(orseq) {  
+  var seq = orseq.clone();
+  if(Math.random() > 0.5){
+    seq.reverse();
+  }
+  var bestv = evaluate(seq);
+  for(var i=0; i<(seq.length>>1); i++) {
+    for(var j=i+2; j<seq.length-1; j++) {
+      var new_seq = swap_seq(seq, i,i+1,j,j+1);
+      var v = evaluate(new_seq);
+      if(v < bestv) {bestv = v, seq = new_seq; };
+    }
+  }
+  //alert(bestv);
+  return seq;
+}
+function preciseMutate1(orseq) {  
+  var seq = orseq.clone();
+  var bestv = evaluate(seq);
+
+  for(var i=0; i<seq.length-1; i++) {
+    var new_seq = seq.clone();
+    new_seq.swap(i, i+1);
+    var v = evaluate(new_seq);
+    if(v < bestv) {bestv = v, seq = new_seq; };
+  }
+  //alert(bestv);
+  return seq;
+}
+function swap_seq(seq, p0, p1, q0, q1) {
+  var seq1 = seq.slice(0, p0);
+  var seq2 = seq.slice(p1+1, q1);
+  seq2.push(seq[p0]);
+  seq2.push(seq[p1]);
+  var seq3 = seq.slice(q1, seq.length);
+  return seq1.concat(seq2).concat(seq3);
+}
+function doMutate(seq) {
+  mutationTimes++;
+  // m and n refers to the actual index in the array
+  // m range from 0 to length-2, n range from 2...length-m
+  do {
+    m = randomNumber(seq.length - 2);
+    n = randomNumber(seq.length);
+  } while (m>=n)
+
+    for(var i=0, j=(n-m+1)>>1; i<j; i++) {
+      seq.swap(m+i, n-i);
+    }
+    return seq;
+}
+function pushMutate(seq) {
+  mutationTimes++;
+  var m,n;
+  do {
+    m = randomNumber(seq.length>>1);
+    n = randomNumber(seq.length);
+  } while (m>=n)
+
+  var s1 = seq.slice(0,m);
+  var s2 = seq.slice(m,n)
+  var s3 = seq.slice(n,seq.length);
+  return s2.concat(s1).concat(s3).clone();
+}
+function setBestValue() {
+  for(var i=0; i<population.length; i++) {
+    values[i] = evaluate(population[i]);
+  }
+  currentBest = getCurrentBest();
+  if(bestValue === undefined || bestValue > currentBest.bestValue) {
+    best = population[currentBest.bestPosition].clone();
+    bestValue = currentBest.bestValue;
+    UNCHANGED_GENS = 0;
+  } else {
+    UNCHANGED_GENS += 1;
+  }
+}
+function getCurrentBest() {
+  var bestP = 0,
+  currentBestValue = values[0];
+
+  for(var i=1; i<population.length; i++) {
+    if(values[i] < currentBestValue) {
+      currentBestValue = values[i];
+      bestP = i;
+    }
+  }
+  return {
+    bestPosition : bestP
+    , bestValue    : currentBestValue
+  }
+}
+function setRoulette() {
+  //calculate all the fitness
+  for(var i=0; i<values.length; i++) { fitnessValues[i] = 1.0/values[i]; }
+  //set the roulette
+  var sum = 0;
+  for(var i=0; i<fitnessValues.length; i++) { sum += fitnessValues[i]; }
+  for(var i=0; i<roulette.length; i++) { roulette[i] = fitnessValues[i]/sum; }
+  for(var i=1; i<roulette.length; i++) { roulette[i] += roulette[i-1]; }
+}
+function wheelOut(rand) {
+  var i;
+  for(i=0; i<roulette.length; i++) {
+    if( rand <= roulette[i] ) {
+      return i;
+    }
+  }
+}
+function randomIndivial(n) {
+  var a = [];
+  for(var i=0; i<n; i++) {
+    a.push(i);
+  }
+  return a.shuffle();
+}
+function evaluate(indivial) {
+  var sum = dis[indivial[0]][indivial[indivial.length - 1]];
+  for(var i=1; i<indivial.length; i++) {
+    sum += dis[indivial[i]][indivial[i-1]];
+  }
+  return sum;
+}
+function countDistances() {
+  var length = points.length;
+  dis = new Array(length);
+  for(var i=0; i<length; i++) {
+    dis[i] = new Array(length);
+    for(var j=0; j<length; j++) {
+      dis[i][j] = ~~distance(points[i], points[j]); 
+    }
+  }
+}
+
+//////////////////
+
+var doPreciseMutate;
+
+var POPULATION_SIZE;
+var ELITE_RATE;
+var CROSSOVER_PROBABILITY;
+var MUTATION_PROBABILITY;
+var OX_CROSSOVER_RATE;
+var UNCHANGED_GENS;
+
+function initData() {
+  running = false;
+  POPULATION_SIZE = 30;
+  ELITE_RATE = 0.3;
+  CROSSOVER_PROBABILITY = 0.9;
+  MUTATION_PROBABILITY  = 0.01;
+  //OX_CROSSOVER_RATE = 0.05;
+  UNCHANGED_GENS = 0;
+  mutationTimes = 0;
+  doPreciseMutate = true;
+
+  bestValue = undefined;
+  best = [];
+  currentGeneration = 0;
+  var currentBest;
+  population = []; //new Array(POPULATION_SIZE);
+  values = new Array(POPULATION_SIZE);
+  fitnessValues = new Array(POPULATION_SIZE);
+  roulette = new Array(POPULATION_SIZE);
+
+    points = dataCoord;
+}
+
+ 
+
+
+
+function drawLines(array) {
+
+  l = lines( points[array[0]].x, points[array[0]].y);
+  for(var i=1; i<array.length; i++) {
+    l.add( points[array[i]].x, points[array[i]].y )
+  }
+  l.add(points[array[0]].x, points[array[0]].y);
+
+  return l;
+}
+
+dataCoord = [ new Point( 78,55), new Point( 115,62), new Point( 99,107), new Point( 182,107), new Point( 72,157), new Point( 132,203), new Point( 198,169), new Point( 193,137), new Point( 238,125), 
+    new Point( 268,139), new Point( 374,191), new Point( 327,142), new Point( 300,124), new Point( 334,107), new Point( 362,83), new Point( 428,95), new Point( 379,109), new Point( 423,151), 
+    new Point( 471,157), new Point( 488,70), new Point( 542,105), new Point( 530,151), new Point( 83,283), new Point( 120,258), new Point( 186,332), new Point( 194,273), new Point( 224,223), 
+    new Point( 109,327), new Point( 129,367), new Point( 265,389), new Point( 233,347), new Point( 273,332), new Point( 253,265), new Point( 355,332), new Point( 271,206), new Point( 238,176), 
+    new Point( 407,379), new Point( 421,424), new Point( 517,391), new Point( 553,444), new Point( 434,307), new Point( 473,334), new Point( 476,285), new Point( 448,245), new Point( 421,203), 
+    new Point( 495,211), new Point( 522,283), new Point( 617,221), new Point( 575,162), new Point( 631,129), new Point( 671,152), new Point( 740,129), new Point( 787,119), new Point( 844,109), 
+    new Point( 886,112), new Point( 911,147), new Point( 958,186), new Point( 940,288), new Point( 914,204), new Point( 812,194), new Point( 711,216), new Point( 763,171), new Point( 716,171), 
+    new Point( 606,412), new Point( 560,384), new Point( 595,359), new Point( 664,302), new Point( 723,288), new Point( 733,248), new Point( 788,266), new Point( 703,377), new Point( 669,337), 
+    new Point( 804,367), new Point( 768,327), new Point( 825,330), new Point( 896,349), new Point( 971,344), new Point( 874,323), new Point( 832,268), new Point( 874,292), new Point( 626,391)];
+
+//////////////////
+
+initData();
+GAInitialize();
+
+setDim( 1024, 650);
 backgroundColor( Color.WHITE);
 
 load( "turkey.png", "turkey");
 
-_gifscript_env.clearTweens( null);
+for ( gen = 1; gen < 350; gen++)
+{
+    GANextGeneration();
 
-drawImage( "turkey", 512, 250);
+    drawImage( "turkey", 512, 250);
 
-frame( 20, false);
+    for(var i=0; i<points.length; i++) {
+        dot(points[i].x, points[i].y, Colors.VenetianRed);
+    }
+
+    drawLines(best);
+
+    write( "Generation: " + currentGeneration, 160, 530);
+    write( "Mutations: " + mutationTimes, 160, 595);
+    write( "Pool population: " + POPULATION_SIZE, 790, 595);
+    write( "Best route: " + ~~(bestValue/249.29*695.83) + " km", 800, 530);
+    
+    frame( 30);
+}
